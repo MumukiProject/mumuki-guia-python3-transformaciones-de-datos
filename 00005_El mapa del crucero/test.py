c7b016bb70c045173b6d978926aefcffd2273e52
franco@mumuki.org <franco@mumuki.org>
2022-10-14 15:47:42 +0000

    Mumuki Export on 2022-10-14 15:47:42 +0000

--- 00005_El mapa del crucero/test.py
+++ 00005_El mapa del crucero/test.py
@@ -5,7 +5,7 @@ class Test(unittest.TestCase):
     
 
   def test_cruceros_de_brasil_son_regionales(self):
-    indexado = muestra_cruceros.set_index("cruise_id")
+    indexado = cruceros.set_index("cruise_id")
     
     self.assertEquals(indexado.loc[10404, "region"], "Regional")
     self.assertEquals(indexado.loc[1494, "region"], "Regional")
@@ -13,19 +13,19 @@ class Test(unittest.TestCase):
     
 
   def test_cruceros_de_argentina_son_regionales(self):
-    indexado = muestra_cruceros.set_index("cruise_id")
+    indexado = cruceros.set_index("cruise_id")
     
     self.assertEquals(indexado.loc[5296, "region"], "Regional")
     self.assertEquals(indexado.loc[12537, "region"], "Regional")
     self.assertEquals(indexado.loc[7237, "region"], "Regional")    
     
   def test_cruceros_de_uruguay_son_nacionales(self):
-    indexado = muestra_cruceros.set_index("cruise_id")
+    indexado = cruceros.set_index("cruise_id")
     
     self.assertEquals(indexado.loc[5823, "region"], "Nacional")     
     
   def test_cruceros_de_usa_son_nan(self):
-    indexado = muestra_cruceros.set_index("cruise_id")
+    indexado = cruceros.set_index("cruise_id")
     
     self.assertTrue(pd.isna(indexado.loc[8829, "region"]))     
         
