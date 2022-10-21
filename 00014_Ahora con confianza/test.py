6a463682d1ed4a2dd606c5bb00ca5512968b146f
franco@mumuki.org <franco@mumuki.org>
2022-10-21 20:14:35 +0000

    Mumuki Export on 2022-10-21 20:14:35 +0000

--- 00014_Ahora con confianza/test.py
+++ 00014_Ahora con confianza/test.py
@@ -14,10 +14,10 @@ class Test(unittest.TestCase):
     self.assertEquals(len(visitas_previas_por_region), 3)
 
   def test_contiene_el_promedio_nacional(self):
-    self.assertEquals(indexado.loc["Nacional", "average_previous_visits"], 3)
+    self.assertEquals(self.indexado.loc["Nacional", "average_previous_visits"], 3)
     
   def test_contiene_el_promedio_regional(self):
-    self.assertEquals(indexado.loc["Regional", "average_previous_visits"], 3)
+    self.assertEquals(self.indexado.loc["Regional", "average_previous_visits"], 3)
     
   def test_contiene_el_promedio_no_regional(self):
-    self.assertEquals(indexado.loc["No regional", "average_previous_visits"], 3)            
+    self.assertEquals(self.indexado.loc["No regional", "average_previous_visits"], 3)            
