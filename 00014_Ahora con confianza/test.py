a75a4143879149db50ae07efa7f7c980677cac71
franco@mumuki.org <franco@mumuki.org>
2022-10-21 19:48:54 +0000

    Mumuki Export on 2022-10-21 19:48:54 +0000

--- 00014_Ahora con confianza/test.py
+++ 00014_Ahora con confianza/test.py
@@ -1,4 +1,8 @@
 class Test(unittest.TestCase):
+  
+  def test_visitas_previas_por_region_es_un_DataFrame(self):
+    self.assertEquals(type(visitas_previas_por_region), pd.DataFrame)
+      
 
   def test_description_example(self):
     self.assertTrue(True)
