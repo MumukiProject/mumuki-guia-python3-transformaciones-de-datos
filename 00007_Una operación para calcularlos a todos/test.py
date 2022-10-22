6e8ded52012f41993cb53e4ad09d7a916ed1b8dd
franco@mumuki.org <franco@mumuki.org>
2022-10-22 01:45:36 +0000

    Mumuki Export on 2022-10-22 01:45:36 +0000

--- 00007_Una operación para calcularlos a todos/test.py
+++ 00007_Una operación para calcularlos a todos/test.py
@@ -1,4 +1,21 @@
 class Test(unittest.TestCase):
 
   def test_ajustar_gastos_con_1_devuelve_una_copia_identica_del_dataframe(self):
-    self.assertTrue(True)
+    original = pd.DataFrame([{
+        'cruise_id': 10404,
+        'country_id': 19,
+        'country': 'Brasil',
+        'date': '2020-02-06',
+        'date_id': 14646,
+        'port_id': 2,
+        'port': 'Punta del Este',
+        'total_people': 2,
+        'visits_amount': 2,
+        'total_expenses': 1.0,
+        'tours_expenses': 5.0,
+        'feed_expenses': 0.0,
+        'transport_expenses': 10.0,
+        'shopping_expenses': 3.0,
+        'other_expenses': 0.0}])
+    self.assertEquals(ajustar_gastos(original), original)
+        
